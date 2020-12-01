15dd34cdb724375cce9364d9d7ea9db0f539e724
stefana-rosca <stefanageorgia05@gmail.com>
2020-12-01 23:38:55 +0200

    first try view

--- src/edu/ubb/Main.java
+++ src/edu/ubb/Main.java
@@ -13,28 +13,42 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main //extends Application {
-{
-/*    @Override
+public class Main extends Application {
+
+
+        @Override
+        public void start(Stage primaryStage) throws Exception{
+            Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
+            primaryStage.setTitle("Hello World");
+            primaryStage.setScene(new Scene(root, 300, 275));
+            primaryStage.show();
+        }
+
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+    }
+  /* @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-    }*/
+    }
 
 
-    public static void main(String[] args) throws IOException {
-        //launch(args);
-        /*JsonReader jsonReader=new JsonReader();
+    public static void main(String[] args) {//throws IOException {
+        launch(args);
+        JsonReader jsonReader=new JsonReader();
         for(Question q: jsonReader.jsonReader())
-            System.out.println(q.getQuestion());*/
+            System.out.println(q.getQuestion());
 
         ControllerQuiz cq=new ControllerQuiz();
         cq.quizGenerator();
         JsonReader jsonReader=new JsonReader();
         for(int i=0; i<26; i++)
-            System.out.println(cq.getQuiz().getQuestions().get(i));
+            System.out.println(cq.getQuiz().getQuestions().get(i));*/
 
-    }
-}
+
+
